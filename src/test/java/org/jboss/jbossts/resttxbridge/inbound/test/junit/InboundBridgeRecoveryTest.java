d29f3ee93840ebda640bf09db0b373bd3fdaa15d
gytist <gytist@gmail.com>
2012-07-02 13:24:50 +0100

    Again uses bridge

--- src/test/java/org/jboss/jbossts/resttxbridge/inbound/test/junit/InboundBridgeRecoveryTest.java
+++ src/test/java/org/jboss/jbossts/resttxbridge/inbound/test/junit/InboundBridgeRecoveryTest.java
@@ -3,6 +3,7 @@ package org.jboss.jbossts.resttxbridge.inbound.test.junit;
 import static org.junit.Assert.assertEquals;
 
 import java.net.HttpURLConnection;
+import java.util.HashMap;
 
 import org.jboss.arquillian.container.test.api.Config;
 import org.jboss.arquillian.junit.Arquillian;
@@ -31,12 +32,13 @@ public class InboundBridgeRecoveryTest extends BaseCrashTest {
         System.out.println("===== testCommit =====");
 
         Config config = new Config();
-        config.add("javaVmArguments", javaVmArguments);
+        config.add("javaVmArguments", javaVmArguments + bytemanArguments);
 
         System.out.println(controller);
         
         controller.start(CONTAINER_NAME, config.map());
         deployer.deploy(DEPLOYMENT_NAME);
+        deployer.deploy(REST_AT_DEPLOYMENT_NAME);
         
         TxSupport txn = new TxSupport(TXN_MGR_URL);
 
@@ -44,50 +46,38 @@ public class InboundBridgeRecoveryTest extends BaseCrashTest {
         txn.startTx();
 
         // Enlist dummy participant
-        String pUrls = TxSupport.getParticipantUrls(DUMMY_URL + "/1", DUMMY_URL + "/1");
-        new TxSupport().httpRequest(new int[] {HttpURLConnection.HTTP_CREATED},
-                txn.txUrl(), "POST", TxSupport.POST_MEDIA_TYPE, pUrls, null);
-        
-        // Enlist second dummy participant
-        pUrls = TxSupport.getParticipantUrls(DUMMY_URL + "/2", DUMMY_URL + "/2");
+        String pUrls = TxSupport.getParticipantUrls(DUMMY_URL, DUMMY_URL);
         new TxSupport().httpRequest(new int[] {HttpURLConnection.HTTP_CREATED},
                 txn.txUrl(), "POST", TxSupport.POST_MEDIA_TYPE, pUrls, null);
         
         // Call to the transactional resource
-//        ClientResponse<String> clientResponse = new ClientRequest(BASE_URL).addLink(
-//                new Link("durableparticipant", "durableparticipant", txn.txUrl(), null, null)).post(String.class);
-//        assertEquals(200, clientResponse.getStatus());
-        
-        System.out.println("Transactions of REST-AT coordinator: " + txn.getTransactions());
+        ClientResponse<String> clientResponse = new ClientRequest(BASE_URL).addLink(
+                new Link("durableparticipant", "durableparticipant", txn.txUrl(), null, null)).post(String.class);
+        assertEquals(200, clientResponse.getStatus());
         
         try {
-            // Commit transaction
             txn.commitTx();
         } catch (HttpResponseException e) {
-            // TODO: handle exception
+            // After crash participant won't return any response and exception will be thrown.
         }
         
         controller.kill(CONTAINER_NAME);
-        controller.start(CONTAINER_NAME);
-        deployer.deploy(DEPLOYMENT_NAME);
         
-        for (int i = 0; i < 120; i++) {
+        config.add("javaVmArguments", javaVmArguments);
+        controller.start(CONTAINER_NAME, config.map());
+        deployer.deploy(DEPLOYMENT_NAME);
+        deployer.deploy(REST_AT_DEPLOYMENT_NAME);
+        
+        // Trigger registration of REST-AT JAX-RS resources
+        System.out.println(txn.getTransactions());
+        
+        // Wait for the recovery
+        for (int i = 0; i < 60*10; i++) {
             Thread.sleep(1000);
             System.out.print(i + " ");
         }
         
-//        clientResponse = new ClientRequest(BASE_URL).addLink(
-//                new Link("durableparticipant", "durableparticipant", txn.txUrl(), null, null)).post(String.class);
-//        assertEquals(200, clientResponse.getStatus());
-        
-        
-//        controller.kill(CONTAINER_NAME);
-//        config.add("javaVmArguments", javaVmArguments);
-//        controller.start(CONTAINER_NAME, config.map());
-//        deployer.undeploy(DEPLOYMENT_NAME);
-//        deployer.deploy(DEPLOYMENT_NAME);
-        
-        
+        deployer.undeploy(REST_AT_DEPLOYMENT_NAME);
         deployer.undeploy(DEPLOYMENT_NAME);
         controller.stop(CONTAINER_NAME);
         controller.kill(CONTAINER_NAME);
