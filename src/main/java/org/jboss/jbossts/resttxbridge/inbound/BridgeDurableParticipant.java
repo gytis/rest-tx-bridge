c36daf555600279e63274ef1d176af300090a610
gytist <gytist@gmail.com>
2012-07-04 13:02:22 +0100

    Fixed inconsistencies with spec

--- src/main/java/org/jboss/jbossts/resttxbridge/inbound/BridgeDurableParticipant.java
+++ src/main/java/org/jboss/jbossts/resttxbridge/inbound/BridgeDurableParticipant.java
@@ -13,7 +13,6 @@ import javax.ws.rs.HEAD;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -26,9 +25,10 @@ import com.arjuna.ats.internal.jta.transaction.arjunacore.jca.SubordinationManag
 
 
 /**
+ * Participant of REST transaction.
+ * Waits for the requests from REST-AT coordinator and terminates mapped JTA transactions.
  * 
  * @author Gytis Trikleris
- * 
  */
 @Path(BridgeDurableParticipant.PARTICIPANT_SEGMENT + "/{participantId}")
 @Participant
@@ -41,19 +41,13 @@ public final class BridgeDurableParticipant {
     public static final String TYPE_IDENTIFIER = "RestTxBridgeDurableParticipant_";
 
     /**
-     * Unique (well, hopefully) formatId so we can distinguish our own Xids.
-     * 
-     * TODO what value should I add here? So far it is taken from WS bridge.
-     */
-    public static final int XARESOURCE_FORMAT_ID = 131080;
-
-    /**
      * Participant URL segment.
      */
     public static final String PARTICIPANT_SEGMENT = "participant-resource";
     
     private static final Logger LOG = Logger.getLogger(BridgeDurableParticipant.class);
 
+    // TODO move to method declarations
     @Context
     private UriInfo uriInfo;
 
@@ -76,6 +70,8 @@ public final class BridgeDurableParticipant {
     }
 
     /**
+     * TODO implement
+     * 
      * Returns current status of the participant.
      * 
      * @return
@@ -85,8 +81,6 @@ public final class BridgeDurableParticipant {
     @GET
     public Response getStatus(@PathParam("participantId") String participantId) {
         System.out.println("BridgeDurableParticipant.getStatus()");
-
-        // TODO check status of the subordinate transaction
 
         return null;
     }
@@ -108,150 +102,138 @@ public final class BridgeDurableParticipant {
         System.out.println("BridgeDurableParticipant.terminateParticipant(). participantId=" + participantId + ", status="
                 + content);
 
-        // TODO check if transaction is active
-
         String txStatus = TxSupport.getStatus(content);
-        String responseStatus = null;
-
+        
         if (TxSupport.isPrepare(txStatus)) {
-            String status = prepare(participantId);
-
-            if (TxSupport.isPrepare(status)) {
-                responseStatus = TxSupport.PREPARED;
-
-            } else if (TxSupport.isReadOnly(status)) {
-                responseStatus = TxSupport.READONLY;
-
-            } else if (TxSupport.isAbort(status)) {
-                responseStatus = TxSupport.ABORTED;
-
-            } else {
-                throw new WebApplicationException(403);
-            }
-
+            return prepare(participantId);
         } else if (TxSupport.isCommit(txStatus)) {
-            commit(participantId);
-            responseStatus = TxSupport.COMMITTED;
-
+            return commit(participantId);
         } else if (txStatus.equals(TxSupport.COMMITTED_ONE_PHASE)) {
-            commitOnePhase(participantId);
-            responseStatus = TxSupport.COMMITTED;
-
+            return commitOnePhase(participantId);
         } else if (TxSupport.isAbort(txStatus)) {
-            rollback(participantId);
-            responseStatus = TxSupport.ABORTED;
+            return rollback(participantId);
         }
-
-        if (responseStatus == null) {
-            return Response.status(HttpURLConnection.HTTP_BAD_REQUEST).build();
-        } else {
-            return Response.ok(TxSupport.toStatusContent(responseStatus)).build();
-        }
+        
+        return Response.status(HttpURLConnection.HTTP_BAD_REQUEST).build();
     }
-
+    
     /**
-     * Prepares subordinate transaction. Returns one of the following statuses: TxSupport.PREPARED TxSupport.READONLY
-     * TxSupport.ABORTED
+     * Prepares subordinate transaction. Returns Response with one of the following statuses: TxSupport.PREPARED
+     * TxSupport.READONLY TxSupport.ABORTED
      * 
      * @param participantId
-     * @return Transaction status
+     * @return Response
      */
-    private String prepare(String participantId) {
-        System.out.println("BridgeDurableParticipant.prepare(participantId=" + participantId + ")");
-
-        String responseStatus = TxSupport.ABORTED;
-
+    private Response prepare(String participantId) {
+        int result = -1;
+        Xid xid;
+        
         try {
-            Xid xid = getXid(participantId);
-            XATerminator xaTerminator = SubordinationManager.getXATerminator();
-
-            int result = xaTerminator.prepare(xid);
-            if (result == XAResource.XA_OK) {
-                responseStatus = TxSupport.PREPARED;
-
-            } else if (result == XAResource.XA_RDONLY) {
-                responseStatus = TxSupport.READONLY;
-            }
-
+            xid = getXid(participantId);
         } catch (Exception e) {
             System.err.println(e.getMessage());
+            return Response.status(409).build();
         }
-
-        if (!TxSupport.isPrepare(responseStatus)) {
-            System.out.println("BridgeDurableParticipant was not prepared. Status=" + responseStatus);
+        
+        XATerminator xaTerminator = SubordinationManager.getXATerminator();
+        
+        try {
+            result = xaTerminator.prepare(xid);
+        } catch (XAException e) {
+            System.err.println(e.getMessage());
+            return Response.status(409).build();
+        }
+        
+        if (result != XAResource.XA_OK) {
+            System.out.println("BridgeDurableParticipant was not prepared. XAResource status=" + result);
             cleanup(participantId);
         }
-
-        return responseStatus;
+        
+        if (result == XAResource.XA_OK) {
+            return Response.ok(TxSupport.toStatusContent(TxSupport.PREPARED)).build();
+        } else if (result == XAResource.XA_RDONLY) {
+            // TODO should DELETE request be executed as mentioned in spec (12p. line 378)
+            return Response.ok(TxSupport.toStatusContent(TxSupport.READONLY)).build();
+        }
+        
+        return Response.ok(TxSupport.toStatusContent(TxSupport.ABORTED)).build();
     }
 
     /**
      * Commits subordinate transaction.
      * 
      * @param participantId
+     * @return Response
      */
-    private void commit(String participantId) {
+    private Response commit(String participantId) {
         System.out.println("BridgeDurableParticipant.commit(participantId=" + participantId + ")");
-
+        
         try {
             Xid xid = getXid(participantId);
             XATerminator xaTerminator = SubordinationManager.getXATerminator();
-
             xaTerminator.commit(xid, false);
-
         } catch (Exception e) {
-            // TODO should aborted status be returned?
             System.err.println(e.getMessage());
+            return Response.status(409).build();
         } finally {
             cleanup(participantId);
         }
+        
+        return Response.ok(TxSupport.COMMITTED).build();
     }
 
     /**
      * Prepares and commits subordinate transaction.
      * 
      * @param participantId
+     * @return Response
      */
-    private void commitOnePhase(String participantId) {
+    private Response commitOnePhase(String participantId) {
         System.out.println("BridgeDurableParticipant.commitOnePhase(participantId=" + participantId + ")");
-
+        
+        boolean commited = false;
         try {
             Xid xid = getXid(participantId);
             XATerminator xaTerminator = SubordinationManager.getXATerminator();
-
             if (xaTerminator.prepare(xid) == XAResource.XA_OK) {
                 commit(participantId);
-            } else {
-                // TODO should aborted status be returned?
-                cleanup(participantId);
+                commited = true;
             }
-
         } catch (Exception e) {
-            // TODO should aborted status be returned?
             System.err.println(e.getMessage());
+        } finally {
             cleanup(participantId);
         }
+        
+        if (commited) {
+            return Response.ok(TxSupport.COMMITTED).build();
+        }
+
+        return Response.status(409).build();
     }
 
     /**
      * Rolls back subordinate transaction.
      * 
      * @param participantId
+     * @return Response
      */
-    private void rollback(String participantId) {
+    private Response rollback(String participantId) {
         System.out.println("BridgeDurableParticipant.rollback(participantId=" + participantId + ")");
-
+        
         try {
             Xid xid = getXid(participantId);
             XATerminator xaTerminator = SubordinationManager.getXATerminator();
-
             xaTerminator.rollback(xid);
 
         } catch (Exception e) {
             System.err.println(e.getMessage());
+            return Response.status(409).build();
         } finally {
             cleanup(participantId);
         }
+        
+        return Response.ok(TxSupport.ABORTED).build();
     }
 
     /**
