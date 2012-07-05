1147bcc75b6750916120059dc284334ee8468be1
gytist <gytist@gmail.com>
2012-07-05 15:45:47 +0100

    Static block to register recovery module and orpah filter added; Sceleton of ClearMapTask added; XA resource format id removed; Comments modified.

--- src/main/java/org/jboss/jbossts/resttxbridge/inbound/InboundBridgeManager.java
+++ src/main/java/org/jboss/jbossts/resttxbridge/inbound/InboundBridgeManager.java
@@ -2,6 +2,8 @@ package org.jboss.jbossts.resttxbridge.inbound;
 
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -11,77 +13,124 @@ import javax.transaction.xa.XAException;
 import javax.transaction.xa.Xid;
 
 import org.jboss.jbossts.star.util.TxSupport;
-import org.jboss.logging.Logger;
-import org.jboss.resteasy.util.Base64;
+//import org.jboss.logging.Logger;
 
 import com.arjuna.ats.arjuna.common.Uid;
+import com.arjuna.ats.arjuna.recovery.RecoveryManager;
+import com.arjuna.ats.arjuna.recovery.RecoveryModule;
+import com.arjuna.ats.internal.jta.recovery.arjunacore.XARecoveryModule;
 import com.arjuna.ats.jta.xa.XATxConverter;
 
 /**
+ * Class to manage inbound bridge instances.
  * 
  * @author Gytis Trikleris
- * 
  */
 public final class InboundBridgeManager {
 
-    private static final Logger LOG = Logger.getLogger(InboundBridgeManager.class);
+    /**
+     * TODO use logger instead of System.out
+     */
+    // private static final Logger LOG = Logger.getLogger(InboundBridgeManager.class);
 
     /**
-     * Mappings of rest transactions and inbound bridges. Map key is URL of the transaction.
+     * Timer to schedule map cleaning task.
+     */
+    private static final Timer timer = new Timer();
+
+    /**
+     * Mappings of rest transactions and inbound bridges. Key - URL of REST-AT; value - Instance of InboundBridge
      */
     private static final ConcurrentMap<String, InboundBridge> inboundBridgeMappings = new ConcurrentHashMap<String, InboundBridge>();
 
     /**
-     * Mappings of participants and transactions. Map key is participant id and value is transaction URL.
+     * Mappings of participants and transactions. Key - participant id; value - URL of REST-AT.
      * 
      * This mapping is required when transaction coordinator is communicating with participant. When request to the participant
-     * is made only participant id is received, therefore we need to get transaction URL.
+     * is made only participant id is received, therefore we need to get transaction URL somehow.
      */
     private static final ConcurrentMap<String, String> participantMappings = new ConcurrentHashMap<String, String>();
 
+    /**
+     * TODO It should be a better place for these actions.
+     * 
+     * Adds inbound bridge orphan filter to the XA recovery module. Adds inbound bridge recovery module to the recovery manager.
+     * Sets timer to execute map clearing task regularly.
+     */
+    static {
+        System.out.println("Deploying recovery module and orphan filter");
+
+        boolean bridgeModuleAdded = false;
+        RecoveryManager recoveryManager = RecoveryManager.manager();
+
+        for (RecoveryModule recoveryModule : recoveryManager.getModules()) {
+            if (recoveryModule instanceof XARecoveryModule) {
+                ((XARecoveryModule) recoveryModule).addXAResourceOrphanFilter(new InboundBridgeOrphanFilter());
+            } else if (recoveryModule instanceof InboundBridgeRecoveryModule) {
+                bridgeModuleAdded = true;
+            }
+        }
+
+        if (!bridgeModuleAdded) {
+            recoveryManager.addModule(new InboundBridgeRecoveryModule());
+        }
+
+        timer.schedule(new ClearMapTask(), 0, 20 * 1000);
+    }
+
+    /**
+     * TODO if rest transaction is associated with new bridge (new request within same transaction was made immediately after
+     * recovery) - notify rest-at coordinator about new participant url
+     * 
+     * Adds inbound bridge to the map.
+     * 
+     * Method is used by inbound bridge recovery manager to enlist recovered bridges.
+     * 
+     * @param inboundBridge
+     * @return boolean
+     */
     public static boolean addInboundBridge(InboundBridge inboundBridge) {
-        /*
-         * TODO if rest transaction is associated with new bridge (new request within same transaction was made
-         * immediately after recovery) - notify rest-at coordinator about new participant url
-         */
-        
         InboundBridge mappedInboundBridge = inboundBridgeMappings.get(inboundBridge.getTxUrl());
-        
+
         if (inboundBridge.equals(mappedInboundBridge)) {
             // Bridge is already mapped
             return true;
         }
-        
+
         if (mappedInboundBridge != null) {
-            // REST-AT trasnaction is already mapped with another bridge
+            // REST-AT is already mapped with another bridge
             return false;
         }
-        
+
         if (participantMappings.containsKey(inboundBridge.getParticipantId())) {
-            // Participant is already mapped with another REST-AT transaction
+            // Participant is already mapped with another REST-AT
             return false;
         }
-        
+
         inboundBridgeMappings.put(inboundBridge.getTxUrl(), inboundBridge);
         participantMappings.put(inboundBridge.getParticipantId(), inboundBridge.getTxUrl());
-        
+
         return true;
     }
-    
+
     /**
+     * Returns inbound bridge for given REST-AT. If inbound bridge mapping does not exist, new bridge with subordinate
+     * transaction and REST-AT participant is created.
+     * 
+     * Base URL is needed when registering bridge's REST-AT participant.
      * 
      * @param txUrl
      * @param baseUrl
-     * @return
-     * @throws IllegalArgumentException
-     * @throws XAException
+     * @return InboundBridge
+     * @throws RollbackException
      * @throws SystemException
-     * @throws RollbackException 
-     * @throws IllegalStateException 
+     * @throws XAException
+     * @throws IllegalStateException
+     * @throws IllegalArgumentException
      */
-    public static InboundBridge getInboundBridge(String txUrl, String baseUrl) throws XAException, SystemException, IllegalStateException, RollbackException {
+    public static InboundBridge getInboundBridge(String txUrl, String baseUrl) throws IllegalStateException, XAException,
+            SystemException, RollbackException {
         System.out.println("InboundBridgeManager.getInboundBridge(txUrl=" + txUrl + ")");
-
         if (txUrl == null) {
             throw new IllegalArgumentException("Transaction URL is required");
         }
@@ -98,9 +147,11 @@ public final class InboundBridgeManager {
     }
 
     /**
+     * Returns URL of REST-AT associated with given participant.
      * 
      * @param participantId
-     * @return
+     * @return String
+     * @throws IllegalArgumentException
      */
     public static synchronized String getParticipantTransaction(String participantId) {
         System.out.println("InboundBridgeManager.getParticipantTransaction(participantId=" + participantId + ")");
@@ -113,9 +164,11 @@ public final class InboundBridgeManager {
     }
 
     /**
+     * Removes mapping of given REST-AT and inbound bridge.
      * 
      * @param txUrl
      * @throws SystemException
+     * @throws IllegalArgumentException
      */
     public static synchronized void removeInboundBridgeMapping(String txUrl) throws SystemException {
         System.out.println("InboundBridgeManager.removeInboundBridgeMapping(txUrl=" + txUrl + ")");
@@ -129,9 +182,13 @@ public final class InboundBridgeManager {
     }
 
     /**
+     * Removes mapping of given participant and REST-AT.
+     * 
+     * Bridge mapping is also removed.
      * 
      * @param participantId
      * @throws SystemException
+     * @throws IllegalArgumentException
      */
     public static synchronized void removeParticipantMapping(String participantId) throws SystemException {
         System.out.println("InboundBridgeManager.removeParticipantMapping(participantId=" + participantId + ")");
@@ -146,13 +203,16 @@ public final class InboundBridgeManager {
     }
 
     /**
+     * Creates new inbound bridge and maps it with given REST-AT.
+     * 
+     * As a result bridge's participant is enlisted with REST-AT.
      * 
      * @param txUrl
      * @param baseUrl
      * @throws XAException
      * @throws SystemException
-     * @throws RollbackException 
-     * @throws IllegalStateException 
+     * @throws RollbackException
+     * @throws IllegalStateException
      */
     private static synchronized void createInboundBridgeMapping(String txUrl, String baseUrl) throws XAException,
             SystemException, IllegalStateException, RollbackException {
@@ -164,7 +224,7 @@ public final class InboundBridgeManager {
         }
 
         // Xid for driving the subordinate transaction
-        Xid xid = XATxConverter.getXid(new Uid(), false, BridgeDurableParticipant.XARESOURCE_FORMAT_ID);
+        Xid xid = XATxConverter.getXid(new Uid(), false, InboundBridge.XARESOURCE_FORMAT_ID);
 
         // construct the participantId in such as way as we can recognize it at recovery time
         String participantId = BridgeDurableParticipant.TYPE_IDENTIFIER + new Uid().fileStringForm();
@@ -175,13 +235,13 @@ public final class InboundBridgeManager {
     }
 
     /**
-     * Enlists participant with REST-AT transaction.
+     * Enlists participant with REST-AT.
      * 
      * @param txUrl
      * @param participantId
      * @param baseUrl
      * @return URL to recover participant
-     * @throws UnsupportedEncodingException 
+     * @throws UnsupportedEncodingException
      */
     private static synchronized void enlistParticipant(String txUrl, String participantId, String baseUrl) {
         System.out.println("InboundBridgeManager.enlistParticipant()");
@@ -191,14 +251,42 @@ public final class InboundBridgeManager {
         }
 
         String participantUrl = baseUrl + BridgeDurableParticipant.PARTICIPANT_SEGMENT + "/" + participantId;
-
-        System.out.println("Participant url: " + participantUrl);
-
         String pUrls = TxSupport.getParticipantUrls(participantUrl, participantUrl);
+        
         new TxSupport().httpRequest(new int[] { HttpURLConnection.HTTP_CREATED }, txUrl, "POST", TxSupport.POST_MEDIA_TYPE,
                 pUrls, null);
-        
+
         participantMappings.put(participantId, txUrl);
+    }
+
+    /**
+     * TODO implement
+     * 
+     * Nested class to execute cleaning of inbound bridges mappings.
+     * 
+     * @author Gytis Trikleris
+     */
+    private static class ClearMapTask extends TimerTask {
+
+        @Override
+        public void run() {
+            System.out.println("InboundBridgeManager.ClearMapTask.run");
+
+            int[] expectedStatusCodes = new int[] { HttpURLConnection.HTTP_NOT_FOUND, HttpURLConnection.HTTP_OK };
+            System.out.println(inboundBridgeMappings);
+            for (String txUrl : inboundBridgeMappings.keySet()) {
+                String response = "";
+
+                try {
+                    response = new TxSupport().httpRequest(expectedStatusCodes, txUrl, "GET", null, null, null);
+                } catch (Exception e) {
+                    continue;
+                }
+
+                String status = TxSupport.getStatus(response);
+                System.out.println(status);
+            }
+        }
     }
 
 }
